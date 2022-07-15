542b4ea2067003f87a6a4feaff973cec17fa2267
Jeniffer Carvalho <jenicarvalho@live.com>
2022-07-15 15:56:00 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>iFrame test</h1>
+      <iframe src="http://localhost:9000/"></iframe>
+    </div>
+  )
+}
+
+export default App
